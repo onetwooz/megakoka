ab26f631a43f2fbfec47565d4beb370658dd224e
onetwooz <63231867+onetwooz@users.noreply.github.com>
2020-04-07 22:59:31 +0500

    Update bbot.py

--- bbot.py
+++ bbot.py
@@ -345,7 +345,7 @@ def send_text(message):
         bot.send_message(message.chat.id, 'https://www.youtube.com/watch?v=AchsISi5178')
     
     else:
-        bot.send_message(message.chat.id, 'Какой урок ты ищешь?') 
+        bot.send_message(message.chat.id, 'Какой урок ты ищешь? У меня есть почти все, с 1 по 111') 
 
         
 bot.polling( none_stop = True )
