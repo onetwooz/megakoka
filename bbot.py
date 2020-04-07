62e9f5a2f21de26e917efb96fca6800bc9921056
onetwooz <63231867+onetwooz@users.noreply.github.com>
2020-04-07 19:02:12 +0500

    Update bbot.py

--- bbot.py
+++ bbot.py
@@ -343,7 +343,7 @@ def send_text(message):
     elif message.text.lower() == "111":
         bot.send_message(message.chat.id, 'https://www.youtube.com/watch?v=AchsISi5178')
         
-        elif message.text.lower() >= "111":
+    elif message.text.lower() >= "111":
         bot.send_message(message.chat.id, 'Сорян, уроков всего 111')
         
 bot.polling( none_stop = True )
