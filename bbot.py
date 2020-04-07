7bd52b17c8d7ad82439ecfe2b0316e2f9ee85ebf
onetwooz <63231867+onetwooz@users.noreply.github.com>
2020-04-08 00:29:59 +0500

    Update bbot.py

--- bbot.py
+++ bbot.py
@@ -11,10 +11,10 @@ def start_message(message):
  
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower() == "1":
+    if message.text.lower() = 1:
         bot.send_message(message.chat.id, 'https://www.youtube.com/watch?v=QA3eSm5uTUY')
     
-    elif message.text.lower() == "2":
+    elif message.text.lower() = 2:
         bot.send_message(message.chat.id, 'https://www.youtube.com/watch?v=-vaCNYqKrJQ&t')
     
     elif message.text.lower() == "3":
