6dede14cd17d5ca813d29daeac6ac39430b0d3f3
onetwooz <63231867+onetwooz@users.noreply.github.com>
2020-04-08 00:35:43 +0500

    Update bbot.py

--- bbot.py
+++ bbot.py
@@ -344,6 +344,9 @@ def send_text(message):
     elif message.text.lower() == "111":
         bot.send_message(message.chat.id, 'https://www.youtube.com/watch?v=AchsISi5178')
         
+    elif message.text.lower() == "Кто тебя создал?":
+        bot.send_message(message.chat.id, '@e_roket - он оч крутой')
+        
     else:
         bot.send_message(message.chat.id, 'Я могу показать тебе урок от 1 до 111') 
 
