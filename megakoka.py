a9450528f54e69b19f89007dfe27b7ceb971162c
onetwooz <63231867+onetwooz@users.noreply.github.com>
2020-04-13 15:46:10 +0500

    Update megakoka.py

--- megakoka.py
+++ megakoka.py
@@ -25,8 +25,8 @@ def send_text(message):
     b = ['a', 's', 'd', 'f']
     c = ['z', 'x', 'c', 'v']
 
-    if message.text.lower() == a[0]:
-        bot.send_message(message.chat.id, c[0])    
+    if message.text.lower() == a[]:
+        bot.send_message(message.chat.id, c[])    
 
     elif message.text.lower() in hi_list:
         bot.send_message(message.chat.id, str(random.choice( hian_list )))
