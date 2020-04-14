2dfe5de9878e496385dcc1d8faa3e3f5c2c45b8d
onetwooz <63231867+onetwooz@users.noreply.github.com>
2020-04-14 21:28:40 +0500

    Update megakoka.py

--- megakoka.py
+++ megakoka.py
@@ -30,7 +30,7 @@ def send_text(message):
         bot.send_message(message.chat.id, descript[minia14.index(message.text.lower())])
        
     elif len(message.text.lower()) == 1:
-        bot.send_message(message.chat.id, "😒")
+        bot.send_message(message.chat.id, str(random.choice( glist )))
 
     elif message.text.lower() in hi_list:
         bot.send_message(message.chat.id, str(random.choice( hian_list )))
