d5244e29b311abc60feac1adfbd039b4b38c0c85
onetwooz <63231867+onetwooz@users.noreply.github.com>
2020-04-14 21:47:58 +0500

    Update megakoka.py

--- megakoka.py
+++ megakoka.py
@@ -29,8 +29,8 @@ def send_text(message):
         bot.send_message(message.chat.id, minia[minia14.index(message.text.lower())])
         bot.send_message(message.chat.id, descript[minia14.index(message.text.lower())])     
        
-    elif len(message.text.lower()) <= 3:
-        bot.send_message(message.chat.id, str(random.choice( glist )))
+    #elif len(message.text.lower()) <= 3:
+        #bot.send_message(message.chat.id, str(random.choice( glist )))
 
     elif message.text.lower() in hi_list:
         bot.send_message(message.chat.id, str(random.choice( hian_list )))
